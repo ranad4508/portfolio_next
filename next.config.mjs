2eab0e4cb1c80e877533177bf5b2e17af5117b1d
Dinesh <ranad4508@gmail.com>
2025-03-07 22:29:50 +0545

    updated configs in wrangler.toml

--- next.config.mjs
+++ next.config.mjs
@@ -15,14 +15,36 @@ const nextConfig = {
   },
   output: "export",
   webpack: (config, { dev, isServer }) => {
-    // Reduce the size of the webpack output
-    config.optimization.minimize = true;
+    // Disable persistent caching
+    config.cache = false;
 
-    // Only include production minimizer
     if (!dev && !isServer) {
-      config.optimization.splitChunks = {
-        chunks: "all",
-        maxSize: 20000000, // 20MB max chunk size
+      // Optimize for size
+      config.optimization = {
+        ...config.optimization,
+        minimize: true,
+        sideEffects: true,
+        // Disable webpack cache
+        runtimeChunk: false,
+        splitChunks: {
+          chunks: "all",
+          maxInitialRequests: Infinity,
+          minSize: 0,
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name(module) {
+                // Get the package name
+                const packageName = module.context.match(
+                  /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+                )[1];
+                // Return a name that won't exceed length limits
+                return `npm.${packageName.replace("@", "")}`;
+              },
+              maxSize: 20000000, // 20MB max chunk size
+            },
+          },
+        },
       };
     }
 
